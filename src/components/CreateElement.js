73b10ca6be6cbdbd3e078de0d91136e36b7efe05
sundarasekar <sundarasekar01@gmail.com>
2023-06-26 17:29:28 +0530

    updated

--- src/components/CreateElement.js
+++ src/components/CreateElement.js
@@ -1,5 +1,10 @@
 import React from 'react'
 
 
-const CreateElement =()=> React.createElement('div',{className:'parentTest'},React.createElement('h1',{id:'test'},'Without JSX - Using React.createElement'))
+const CreateElement =()=>
+
+ React.createElement('div',{className:'parentTest'},React.createElement('h1',{id:'test'},'Without JSX - Using React.createElement'))
+
 export default CreateElement
+
+
