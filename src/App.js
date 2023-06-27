f391d8915434cd05857f7b6bb6cb5700f1bcb4cd
sundarasekar <sundarasekar01@gmail.com>
2023-06-27 07:44:15 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import Greet from './components/Functional'
-import Welcome from './components/Class'
+import Greet from './components/Cevolution/Functional'
+import Welcome from './components/Cevolution/Class'
 import './App.css'
-import CreateElement from './components/CreateElement';
-import PropsFunc from './components/PropsFunc'
-import PropsClass from './components/PropsClass'
-import StateClass from './components/StateClass'
-import Counter from './components/Counter'
-import EventHandler from './components/EventHandler'
-import ParentComp from './components/parentComp'
-import ConditionalRender from './components/ConditionalRender';
+import CreateElement from './components/Cevolution/CreateElement';
+import PropsFunc from './components/Cevolution/PropsFunc'
+import PropsClass from './components/Cevolution/PropsClass'
+import StateClass from './components/Cevolution/StateClass'
+import Counter from './components/Cevolution/Counter'
+import EventHandler from './components/Cevolution/EventHandler'
+import ParentComp from './components/Cevolution/parentComp'
+import ConditionalRender from './components/Cevolution/ConditionalRender';
 import User from './components/Dip/useState'
 
 function App() {
