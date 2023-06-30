acaa419b3b25758a1e9b411d9200c71e472e4cc2
sundarasekar <sundarasekar01@gmail.com>
2023-06-30 15:56:12 +0530

    updated

--- src/components/CodeEvolution/PropsFunc.js
+++ src/components/CodeEvolution/PropsFunc.js
@@ -3,16 +3,10 @@
  
 // const Functional = ({FirstName,LastName})=>{
 
-const Functional = props =>{
-
-
  //Destructuring Inside Function body
- const {FirstName,LastName} = props
- return <h1>Props in FunctionalComp - {FirstName}{LastName}</h1>
 
-//Common Method
 // return <h1>Props in FunctionalComp - {props.FirstName}{props.LastName}</h1>
 
-}
+
 
 export default Functional
